de032a3fd6658bb58d93c01ec40984eef7076ec6
DANG QUOC DAT <dat03122003@gmail.com>
2025-05-15 20:24:06 +0700

    Add API getAllClassesByFaculty

--- src/main/java/com/quanlydoantotnghiep/DoAnTotNghiep/controller/ClassController.java
+++ src/main/java/com/quanlydoantotnghiep/DoAnTotNghiep/controller/ClassController.java
@@ -4,6 +4,7 @@ import com.quanlydoantotnghiep.DoAnTotNghiep.service.ClassService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,13 @@ public class ClassController {
     public ResponseEntity<?> getAllClasses() {
 
         return ResponseEntity.ok(classService.getAllClasses());
+    }
+
+    @GetMapping("/faculty/{facultyId}")
+    public ResponseEntity<?> getAllClassesByFaculty(
+            @PathVariable("facultyId") Long facultyId
+    ) {
+
+        return ResponseEntity.ok(classService.getAllClassesByFacultyId(facultyId));
     }
 }
